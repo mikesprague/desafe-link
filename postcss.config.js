3b6ff06705ccaed24a7e2f81a396454b42a8ba2d
Michael Sprague <mikesprague@gmail.com>
2024-11-22 12:57:40 -0500

    chore: update config

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import tailwindcss from 'tailwindcss';
+
+// safelist any necessary classes here
+const cssSafelistClassArray = [];
+
+export default {
+  plugins: [
+    autoprefixer,
+    tailwindcss,
+    cssnano({
+      preset: 'default',
+    }),
+    purgeCSSPlugin({
+      content: [
+        './src/index.html',
+        './src/index.js',
+        './src/components/**/*.jsx',
+      ],
+      fontFace: false,
+      safelist: cssSafelistClassArray,
+    }),
+  ],
+};
